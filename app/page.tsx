0a5e851c64cc416a6d6828b41baa746584133131
Isha Mahajan <mahajanisha508@gmail.com>
2024-07-09 19:23:06 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+<h3 className="text-white">Home </h3>
+<Button>hello world</Button>
+    </main>
+  );
+}
